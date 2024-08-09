0cf148f79d066216387d6ec27b78db695ed31708
Dafina Trufas <dafinatrufas@gmail.com>
2024-08-09 11:21:05 +0300

    Updated IL.lean

--- IL.lean
+++ IL.lean
@@ -1,3 +1,10 @@
 -- This module serves as the root of the `Il` library.
 -- Import modules here that should be built as part of the library.
-import Il.Basic
+import Il.Formula
+import Il.Syntax
+import Il.Semantics
+import Il.Soundness
+import Il.CompletenessListUtils
+import Il.Completeness
+import Il.HeytingAlgebraUtils
+import Il.HeytingAlgebraSemantics
